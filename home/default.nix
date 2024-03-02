953ee07f9e80ef20114599e5888f148e874393e5
Michael Andreas Graversen <home@michael-graversen.dk>
2024-03-02 21:18:15 +0100

    youtube music as webapp standalone

--- home/default.nix
+++ home/default.nix
@@ -3,6 +3,7 @@
   pkgs,
   inputs,
   theme,
+  lib,
   ...
 }: {
   # TODO setup nixd autocomplete
@@ -45,7 +46,7 @@
 
   home.file = {
     "Pictures/wallpapers" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/wallpapers";
+      source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/wallpapers";
     };
   };
 
@@ -59,6 +60,29 @@
   programs.thefuck.enable = true;
 
   xdg.configFile."Vencord/settings/quickCss.css".text = theme.discordCss;
+
+  # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/applications/video/netflix/default.nix#L17
+  # https://github.com/mathix420/free-the-web-apps/blob/master/apps/youtube-music/youtube-music.desktop
+  # https://www.reddit.com/r/unixporn/comments/1aup8kn/oc_unleash_the_power_of_web_apps_forget_about/
+  xdg.desktopEntries.youtube-music = {
+    type = "Application";
+    name = "Youtube Music";
+    genericName = "Music player";
+    comment = "A new music service with official albums, singles, videos, remixes, live performances and more for Android, iOS and desktop. It's all here.";
+    exec = "${pkgs.google-chrome}/bin/${pkgs.google-chrome.meta.mainProgram} --app=https://music.youtube.com/ --no-first-run --no-default-browser-check --no-crash-upload \"\\$@\""; # TODO: switch to ungoogled-chromium
+    # icon = ./youtube-music.png;
+    icon = pkgs.fetchurl {
+      name = "youtube-music-icon.png";
+      url = "https://music.youtube.com/img/favicon_144.png"; # TODO: find higher res source or just save in repo
+      sha256 = "sha256-xuHQU1LBXb8ATf7uZ+Jz/xnASyzWlMkBfJgn6NjZz1Y=";
+      meta.license = lib.licenses.unfree;
+    };
+    categories = ["AudioVideo" "Player"];
+    startupNotify = true;
+    settings = {
+      StartupWMClass = "youtube-music";
+    };
+  };
 
   home.packages = with pkgs; [
     # System
@@ -113,7 +137,7 @@
     vlc
     spotify
     spotify-player
-    sunshine
+    sunshine # https://docs.lizardbyte.dev/projects/sunshine/en/latest/about/usage.html#setup
     g4music
 
     # Image viewer

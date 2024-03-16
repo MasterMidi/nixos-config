c43fb42de15459b0831393b1a3fe29c5c29b303a
Michael Andreas Graversen <home@michael-graversen.dk>
2024-03-16 09:05:34 +0100

    added ags configs for inspiration

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,23 +1,23 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-if [ "$EUID" -ne 0 ]
-  then echo "󰣮 Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+	echo "󰣮 Please run as root"
+	exit
 fi
 
 echo "${bold}⚗️  Testing Configuration ${normal}"
 case "$OSTYPE" in
-  linux*)   sudo nixos-rebuild dry-build;;
-  *)        echo "unknown: $OSTYPE" ;;
+linux*) sudo nixos-rebuild dry-build ;;
+*) echo "unknown: $OSTYPE" ;;
 esac
 
 if [ $? -eq 0 ]; then
 	echo "🔨 Rebuilding Configuration…"
 	case "$OSTYPE" in
-	  linux*)   sudo nixos-rebuild switch;;
-	  *)        echo "unknown: $OSTYPE" ;;
+	linux*) sudo nixos-rebuild switch ;;
+	*) echo "unknown: $OSTYPE" ;;
 	esac
 	if [ $? -eq 0 ]; then
 		echo "❄️ Done! NixOS is Now Running Generation $(sudo nix-env --list-generations --profile /nix/var/nix/profiles/system | grep current | awk '{print $1}')."

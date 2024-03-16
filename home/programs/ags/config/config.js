d578632b2f113f569f4e63d11b58376cb4c1c21d
Michael Andreas Graversen <home@michael-graversen.dk>
2024-03-16 17:02:10 +0100

    clean out ags

--- home/programs/ags/config/config.js
+++ home/programs/ags/config/config.js
@@ -1,27 +1,13 @@
-const main = "/tmp/ags/main.js"
-const entry = `${App.configDir}/main.ts`
+const myLabel = Widget.Label({
+	label: 'some example content',
+})
 
-const v = {
-    ags: pkg.version.split(".").map(Number),
-    expect: [1, 8, 0],
-}
+const myBar = Widget.Window({
+	name: 'bar',
+	anchor: ['top', 'left', 'right'],
+	child: myLabel,
+})
 
-try {
-    await Utils.execAsync([
-        "bun", "build", entry,
-        "--outfile", main,
-        "--external", "resource://*",
-        "--external", "gi://*",
-        "--external", "file://*",
-    ])
-    if (v.ags[1] < v.expect[1] || v.ags[2] < v.expect[2]) {
-        print(`my config needs at least v${v.expect.join(".")}, yours is v${v.ags.join(".")}`)
-        App.quit()
-    }
-    await import(`file://${main}`)
-} catch (error) {
-    console.error(error)
-    App.quit()
-}
-
-export { }
+App.config({
+	windows: [myBar]
+})

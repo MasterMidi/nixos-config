14015fd8be0cd334653bce9a83582439412fae91
Michael Andreas Graversen <home@michael-graversen.dk>
2024-03-16 21:44:10 +0100

    added mimetypes

--- home/defaultApps.nix
+++ home/defaultApps.nix
@@ -2,64 +2,101 @@
   browser = "firefox.desktop";
   imageViewer = "pix.desktop";
   audioPlayer = "org.gnome.rhythmbox3.desktop";
+  videoPlayer = "mpv.desktop";
+
+  generateMappings = {
+    type,
+    extensions,
+    viewer,
+  }: let
+    makePair = ext: {"${type}/${ext}" = viewer;};
+    mergeAttrs = list: builtins.foldl' (acc: elem: acc // elem) {} list;
+  in
+    mergeAttrs (map makePair extensions);
 in {
   xdg.mimeApps = {
     enable = true;
-    defaultApplications = {
-      "image/gif" = [imageViewer];
-      "application/x-extension-shtml" = browser;
-      "application/xhtml+xml" = browser;
-      "application/x-extension-xhtml" = browser;
-      "application/x-extension-xht" = browser;
-      "image/jpeg" = imageViewer;
-      "audio/x-vorbis+ogg" = audioPlayer;
-      "audio/vorbis" = audioPlayer;
-      "audio/x-vorbis" = audioPlayer;
-      "audio/x-scpls" = audioPlayer;
-      "audio/x-mp3" = audioPlayer;
-      "audio/x-mpeg" = audioPlayer;
-      "audio/x-mpegurl" = audioPlayer;
-      "audio/x-flac" = audioPlayer;
-      "audio/mp4" = audioPlayer;
-      "audio/x-it" = audioPlayer;
-      "audio/x-mod" = audioPlayer;
-      "audio/x-s3m" = audioPlayer;
-      "audio/x-stm" = audioPlayer;
-      "audio/x-xm" = audioPlayer;
-      "image/png" = imageViewer;
-      "image/webp" = imageViewer;
-      "image/tiff" = imageViewer;
-      "image/x-tga" = imageViewer;
-      "image/vnd-ms.dds" = imageViewer;
-      "image/x-dds" = imageViewer;
-      "image/bmp" = imageViewer;
-      "image/vnd.microsoft.icon" = imageViewer;
-      "image/vnd.radiance" = imageViewer;
-      "image/x-exr" = imageViewer;
-      "image/x-portable-bitmap" = imageViewer;
-      "image/x-portable-graymap" = imageViewer;
-      "image/x-portable-pixmap" = imageViewer;
-      "image/x-portable-anymap" = imageViewer;
-      "image/x-qoi" = imageViewer;
-      "image/svg+xml" = imageViewer;
-      "image/svg+xml-compressed" = imageViewer;
-      "image/avif" = imageViewer;
-      "image/heic" = imageViewer;
-      "image/jxl" = imageViewer;
-      "x-scheme-handler/ror2mm" = "r2modman.desktop";
-    };
+    defaultApplications =
+      generateMappings {
+        type = "application";
+        extensions = [
+          "x-extension-shtml"
+          "x-extension-xht"
+          "x-extension-xhtml"
+          "xhtml+xml"
+        ];
+        viewer = browser;
+      }
+      // generateMappings {
+        type = "audio";
+        extensions = [
+          "vorbis"
+          "x-flac"
+          "x-it"
+          "x-mod"
+          "x-mp3"
+          "x-mpeg"
+          "x-mpegurl"
+          "x-s3m"
+          "x-scpls"
+          "x-stm"
+          "x-vorbis"
+          "x-vorbis+ogg"
+          "x-xm"
+        ];
+        viewer = audioPlayer;
+      }
+      // generateMappings {
+        type = "image";
+        extensions = [
+          "avif"
+          "bmp"
+          "gif"
+          "heic"
+          "jpeg"
+          "jxl"
+          "png"
+          "svg+xml-compressed"
+          "svg+xml"
+          "tiff"
+          "vnd-ms.dds"
+          "vnd.microsoft.icon"
+          "vnd.radiance"
+          "webp"
+          "x-dds"
+          "x-exr"
+          "x-portable-anymap"
+          "x-portable-bitmap"
+          "x-portable-graymap"
+          "x-portable-pixmap"
+          "x-qoi"
+          "x-tga"
+        ];
+        viewer = imageViewer;
+      }
+      // generateMappings {
+        type = "video";
+        extensions = [
+          "x-matroska"
+          "mp4"
+        ];
+        viewer = videoPlayer;
+      }
+      // generateMappings {
+        type = "x-scheme-handler";
+        extensions = ["ror2mm"];
+        viewer = "r2modman.desktop";
+      };
     associations.added = {
       "x-scheme-handler/mailto" = "userapp-Thunderbird-L02IE2.desktop";
       "x-scheme-handler/mid" = "userapp-Thunderbird-L02IE2.desktop";
       "x-scheme-handler/webcal" = "userapp-Thunderbird-TJPIE2.desktop";
       "x-scheme-handler/webcals" = "userapp-Thunderbird-TJPIE2.desktop";
-      "video/x-matroska" = "vlc.desktop";
       "audio/flac" = audioPlayer;
       "x-scheme-handler/chrome" = browser;
       "application/x-extension-htm" = browser;
       "application/x-extension-html" = browser;
       "text/html" = browser;
-      "video/mp4" = "vlc.desktop";
     };
   };
 }

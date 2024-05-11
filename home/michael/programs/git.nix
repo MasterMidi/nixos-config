1f4ecfbb0b089030e7843d4044abf2ee4f25403a
Michael Andreas Graversen <work@michael-graversen.dk>
2024-05-11 10:14:11 +0200

    update git to ssh

--- home/michael/programs/git.nix
+++ home/michael/programs/git.nix
@@ -2,13 +2,13 @@
   programs.git = {
     enable = true;
     userName = "Michael Andreas Graversen";
-    userEmail = "home@michael-graversen.dk";
+    userEmail = "work@michael-graversen.dk";
     lfs.enable = true;
     extraConfig = {
       credential = {
         credentialStore = "secretservice";
         # helper = "${pkgs.git-credential-manager}/lib/git-credential-manager/git-credential-manager";
-        helper = "${pkgs.nur.repos.utybo.git-credential-manager}/bin/git-credential-manager"; # TODO: don't use nur repo
+        # helper = "${pkgs.nur.repos.utybo.git-credential-manager}/bin/git-credential-manager"; # TODO: don't use nur repo
       };
       pull.rebase = false;
     };

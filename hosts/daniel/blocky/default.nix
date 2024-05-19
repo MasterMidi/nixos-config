168d62b619c3d706f663ca715811067f07033ae3
Michael Andreas Graversen <work@michael-graversen.dk>
2024-05-19 16:16:01 +0200

    add blocky

---
+++ hosts/daniel/blocky/default.nix
@@ -0,0 +1,36 @@
+{...}:
+{
+     virtualisation.containers.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+    enableOnBoot = true;
+  };
+
+  virtualisation.oci-containers.backend = "docker";
+
+  virtualisation.oci-containers.containers = {
+    blocky = {
+      image = "spx01/blocky";
+      autoStart= true;
+      # Optional the instance hostname for logging purpose
+      hostname= "blocky";
+      ports = [
+        "53:53/tcp"
+        "53:53/udp"
+        "4000:4000/tcp"];
+      environment= {
+        TZ="Europe/Copenhagen"; # Optional to synchronize the log timestamp with host
+      };
+      volumes = [
+        # Optional to synchronize the log timestamp with host
+        "/etc/localtime:/etc/localtime:ro"
+        # config file
+        "${./blocklist.yml}:/app/config.yml"
+        ];
+    };
+  };
+}

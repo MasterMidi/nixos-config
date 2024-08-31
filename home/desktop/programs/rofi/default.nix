9e22baf043262c16c3d15f61085f70d676ac4f0c
Michael Andreas Graversen <home@michael-graversen.dk>
2024-08-31 22:53:45 +0200

    cleanup unsused rofi stuff

--- home/desktop/programs/rofi/default.nix
+++ home/desktop/programs/rofi/default.nix
@@ -91,6 +91,7 @@ in {
   imports = [
     ./wallpaper-switcher
     ./clipboard.nix
+    ./gamelauncher.nix
   ];
 
   programs.rofi = {
@@ -127,19 +128,8 @@ in {
     bemoji
     bitwarden-menu
 
-    (writeShellScriptBin "rofi-games" (builtins.readFile ./gamelauncher/gamelauncher.sh))
     (writeShellScriptBin "rofi-network" (builtins.readFile ./rofi-network-manager/rofi-network-manager.sh))
     (writeShellScriptBin "rofi-bitwarden" (builtins.readFile ./rofi-bitwarden.sh))
-    # (writeShellScriptBin "rofi-emoji" (builtins.readFile ./rofi-emoji.sh))
-    # (writeShellScriptBin "rofi-kaomoji" (builtins.readFile (
-    #   pkgs.substituteAll {rofi-emoji
-    #     src = ./rofi-kaomoji.sh;
-    #     isExecutable = true;
-
-    #     # Substitutions
-    #     kaomojis = ./kaomoji.txt;
-    #   }
-    # )))
   ];
 
   xdg.configFile = {

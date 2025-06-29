e5b03185b70442ff510bfad4406ecafcf1a3e88b
Michael Andreas Graversen <home@michael-graversen.dk>
2025-06-29 14:41:30 +0200

    enable ollama with rocm support

--- machines/desktops/jason/services/default.nix
+++ machines/desktops/jason/services/default.nix
@@ -1,6 +1,26 @@
-{...}: {
+{ pkgs, ... }:
+{
   services.ollama = {
-    enable = false;
-    port = "11434";
+    enable = true;
+    port = 11434;
+    host = "0.0.0.0"; # TODO: only listen on localhost and tailscale network
+    openFirewall = true;
+    acceleration = "rocm";
   };
+
+  systemd.tmpfiles.rules =
+    let
+      rocmEnv = pkgs.symlinkJoin {
+        name = "rocm-combined";
+        paths = with pkgs.rocmPackages; [
+          rocblas
+          hipblas
+          clr
+        ];
+      };
+    in
+    [
+      "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
+    ];
+
 }

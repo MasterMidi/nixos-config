4c433ee7930576a4b542d233068d95764e74a937
Michael Andreas Graversen <home@michael-graversen.dk>
2025-11-21 08:56:48 +0000

    dont set a default sops file

--- users/michael/common/secrets/default.nix
+++ users/michael/common/secrets/default.nix
@@ -2,16 +2,17 @@
 {
   imports = [ inputs.sops-nix.homeManagerModules.sops ];
   sops = {
-    defaultSopsFile = ./secrets.yaml;
     age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
     secrets = {
       SSH_KEY = {
         mode = "0600";
         path = "${config.home.homeDirectory}/.ssh/id_ed25519";
+        sopsFile = ./secrets.yaml;
       };
       SSH_KEY_PUB = {
         mode = "0600";
         path = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+        sopsFile = ./secrets.yaml;
       };
     };
   };

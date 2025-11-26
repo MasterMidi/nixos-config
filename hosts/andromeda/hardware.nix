547000e7e490bd823de520f924ccd7d5154e96ca
Michael Andreas Graversen <work@michael-graversen.dk>
2025-11-26 14:09:44 +0100

    Update file system options and add containers mount
    Removed automount options for SSD and HDD file systems and added a bind mount for containers.

--- hosts/andromeda/hardware.nix
+++ hosts/andromeda/hardware.nix
@@ -32,18 +32,16 @@
   fileSystems."/mnt/ssd" = {
     device = "/dev/disk/by-uuid/2acaf814-d46b-45a6-a679-c0bf467a2457";
     fsType = "xfs";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-    ];
   };
 
   fileSystems."/mnt/hdd" = {
     device = "/dev/disk/by-uuid/9d66d39f-6f56-4d40-a768-0773e4db01b3";
     fsType = "xfs";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-    ];
+  };
+
+  fileSystems."/var/lib/containers" = {
+    device = "/mnt/data/containers";
+    fsType = "none";
+    options = [ "bind" ];
   };
 }

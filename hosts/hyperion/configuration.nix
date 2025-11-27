77f82d09c4e16fb1e8d71f758a2160483fce1220
Michael Andreas Graversen <home@michael-graversen.dk>
2025-11-27 11:12:21 +0000

    fix tailscale magicdns in wsl

--- hosts/hyperion/configuration.nix
+++ hosts/hyperion/configuration.nix
@@ -16,12 +16,15 @@
     wslConf.automount.root = "/mnt";
     wslConf.interop.appendWindowsPath = false;
     wslConf.network.generateHosts = false;
+    wslConf.network.generateResolvConf = false;
     defaultUser = "michael";
     startMenuLaunchers = true;
 
     # Enable integration with Docker Desktop (needs to be installed)
     docker-desktop.enable = true;
   };
+
+  services.resolved.enable = true;
 
   virtualisation.docker = {
     enable = true;

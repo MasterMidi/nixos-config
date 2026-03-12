76f95809da430878371c6e7f9b20eae0a5c6a76d
Michael Andreas Graversen <home@michael-graversen.dk>
2026-03-12 08:39:47 +0100

    yazi preview markdown files

--- users/common/programs/yazi.nix
+++ users/common/programs/yazi.nix
@@ -1,27 +1,36 @@
 { pkgs, ... }:
 {
-	programs.yazi = {
-		enable = true;
-		enableBashIntegration = true;
-		shellWrapperName = "y"; # use `y` to get cd-on-quit behaviour
+  programs.yazi = {
+    enable = true;
+    enableBashIntegration = true;
+    shellWrapperName = "y"; # use `y` to get cd-on-quit behaviour
 
-		extraPackages = with pkgs; [
-			ffmpegthumbnailer # video thumbnails
-			poppler-utils # PDF previews
-			imagemagick # image conversion/previews
-			file # MIME type detection
-			p7zip # archive previews
-		];
+    extraPackages = with pkgs; [
+      ffmpegthumbnailer # video thumbnails
+      poppler-utils # PDF previews
+      imagemagick # image conversion/previews
+      file # MIME type detection
+      p7zip # archive previews
+    ];
 
-		settings = {
-			manager = {
-				show_hidden = false;
-				sort_by = "natural";
-				sort_dir_first = true;
-			};
-			preview = {
-				image_quality = 90;
-			};
-		};
-	};
+    settings = {
+      manager = {
+        show_hidden = false;
+        sort_by = "natural";
+        sort_dir_first = true;
+      };
+      preview = {
+        image_quality = 90;
+      };
+      plugin.prepend-previewers = builtins.attrValues {
+        markdown-preview = {
+          url = "*.md";
+          run = "piper -- CLICOLOR_FORCE=1 ${pkgs.lib.getExe pkgs.glow} -w=\$w -s=dark";
+        };
+      };
+    };
+    plugins = {
+      piper = pkgs.yaziPlugins.piper;
+    };
+  };
 }

6a3a2394927aad33f63935e950d6650af31471ec
Michael Andreas Graversen <home@michael-graversen.dk>
2026-04-07 18:36:28 +0200

    Fix syntax error

--- k8s/longhorn.nix
+++ k8s/longhorn.nix
@@ -31,33 +31,35 @@
     };
 
     StorageClass.longhorn-database = {
-    metadata.annotations.storageclass."kubernetes.io/is-default-class" = false;
-    provisioner = "driver.longhorn.io";
-    allowVolumeExpansion = true;
-    reclaimPolicy = "Retain";
-    volumeBindingMode = "Immediate";
-    parameters = {
-      numberOfReplicas = "1";
-      dataLocality = "best-effort";
-      fsType = "xfs";
-      staleReplicaTimeout = "2880";
-      # fromBackup = "";
-      diskSelector = "ssd,fast";
-      # mkfsParams = "-I 256 -b 4096 -O ^metadata_csum,^64bit";
-      # backingImage = "bi-test";
-      # backingImageDataSourceType = "download";
-      # backingImageDataSourceParameters = "{\"url\": \"https://backing-image-example.s3-region.amazonaws.com/test-backing-image\"}";
-      # backingImageChecksum = "SHA512 checksum of the backing image";
-      # unmapMarkSnapChainRemoved = "ignored";
-      # nodeSelector = "storage,fast";
-      # recurringJobSelector = "[{\"name\":\"snap-group\", \"isGroup\":true}, {\"name\":\"backup\", \"isGroup\":false}]";
-      # nfsOptions = "soft,timeo=150,retrans=3";
+      metadata.annotations.storageclass."kubernetes.io/is-default-class" = false;
+      provisioner = "driver.longhorn.io";
+      allowVolumeExpansion = true;
+      reclaimPolicy = "Retain";
+      volumeBindingMode = "Immediate";
+      parameters = {
+        numberOfReplicas = "1";
+        dataLocality = "best-effort";
+        fsType = "xfs";
+        staleReplicaTimeout = "2880";
+        # fromBackup = "";
+        diskSelector = "ssd,fast";
+        # mkfsParams = "-I 256 -b 4096 -O ^metadata_csum,^64bit";
+        # backingImage = "bi-test";
+        # backingImageDataSourceType = "download";
+        # backingImageDataSourceParameters = "{\"url\": \"https://backing-image-example.s3-region.amazonaws.com/test-backing-image\"}";
+        # backingImageChecksum = "SHA512 checksum of the backing image";
+        # unmapMarkSnapChainRemoved = "ignored";
+        # nodeSelector = "storage,fast";
+        # recurringJobSelector = "[{\"name\":\"snap-group\", \"isGroup\":true}, {\"name\":\"backup\", \"isGroup\":false}]";
+        # nfsOptions = "soft,timeo=150,retrans=3";
+      };
+      mountOptions = [
+        "noatime" # Don't update file access times
+        "nodiratime" # Don't update directory access times
+      ];
     };
-    mountOptions = [
-      "noatime" # Don't update file access times
-      "nodiratime" # Don't update directory access times
-    ];
   };
+
   helm.releases.longhorn = {
     namespace = "longhorn-system";
     chart = pkgs.fetchHelm {

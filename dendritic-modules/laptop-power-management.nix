54b4e72cd45c1cf78d43690b3fc7eea402a2841c
Michael Andreas Graversen <home@michael-graversen.dk>
2026-04-07 21:46:37 +0200

    add module for zswap and use on laptop

--- dendritic-modules/laptop-power-management.nix
+++ dendritic-modules/laptop-power-management.nix
@@ -1,31 +1,22 @@
-{ ... }:
+{ self, ... }:
 {
   flake.nixosModules.laptop-power-management =
     { config, lib, ... }:
     {
+      imports = [ self.nixosModules.zswap ];
+
+      zswap = {
+        enable = true;
+        compressor = "lzo";
+      };
+
       boot = {
         # Define kernel parameters for zswap and hibernation
         kernelParams = [
-          "zswap.enabled=1"
-          "zswap.compressor=lz4"
-          "zswap.max_pool_percent=20"
-          "zswap.zpool=z3fold" # z3fold allows up to 3 compressed pages per physical page (better than default zbud)
-          "zswap.shrinker_enabled=1"
-
           # WARNING: resume_offset is highly specific to the physical location of the swapfile on the disk.
           # If this flake module is shared across multiple machines, this value MUST be moved to a host-specific file.
           "resume_offset=24452618"
         ];
-
-        # Ensure the kernel has the necessary modules to perform the zswap compression and pooling
-        kernelModules = [
-          "lz4"
-          "lz4_compress"
-          "z3fold"
-        ];
-
-        # systemd in initrd is highly recommended for modern NixOS, especially for reliable resume from hibernation.
-        initrd.systemd.enable = true;
 
         # The block device that contains the swapfile.
         resumeDevice = config.fileSystems."/".device;

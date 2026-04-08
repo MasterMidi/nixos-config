8697e8c61efbf4c28c9f84af67c9b784237e89ba
Michael Andreas Graversen <home@michael-graversen.dk>
2026-04-08 11:07:18 +0000

    enable jj config hyperion

--- hosts/hyperion/default.nix
+++ hosts/hyperion/default.nix
@@ -27,6 +27,7 @@
           home-manager.users.michael.imports = [
             self.homeModules.k8s-cluster-administration
             self.homeModules.opencode
+            self.homeModules.jujutsu
           ];
         }
       ];

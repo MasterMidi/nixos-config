885d9de2c9d07239f916f12ad599234de9daf367
Michael Andreas Graversen <home@michael-graversen.dk>
2026-06-15 17:42:15 +0200

    fix jjui askpass problem

--- dendritic-modules/programs/jujutsu.nix
+++ dendritic-modules/programs/jujutsu.nix
@@ -6,7 +6,10 @@
       programs.jujutsu = {
         enable = true;
         settings = {
-          user = { name = "Michael Andreas Graversen"; email = "home@michael-graversen.dk";};
+          user = {
+            name = "Michael Andreas Graversen";
+            email = "home@michael-graversen.dk";
+          };
           template-aliases."format_short_id(id)" = "id.shortest()";
         };
       };
@@ -15,6 +18,7 @@
         enable = true;
         settings = {
           preview.show_at_start = true;
+          ssh.hijack_askpass = true;
         };
       };
     };

f938786518875ee5490fd613d84c00d3b25df29c
Michael Andreas Graversen <home@michael-graversen.dk>
2026-06-28 10:57:35 +0200

    chore: update qui to version 1.21.0

--- k8s/qui.nix
+++ k8s/qui.nix
@@ -41,7 +41,7 @@ in
             containers = {
               _namedlist = true;
               qui = {
-                image = "ghcr.io/autobrr/qui:v1.20.0";
+                image = "ghcr.io/autobrr/qui:v1.21.0";
                 ports = {
                   _namedlist = true;
                   http.containerPort = 7476;

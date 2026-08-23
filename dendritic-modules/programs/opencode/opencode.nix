4e18c8241716b23c55a7dc0858ba478de284fa42
Michael Andreas Graversen <home@michael-graversen.dk>
2026-08-23 09:27:38 +0200

    add openspec 1.10 to opencode

--- dendritic-modules/programs/opencode/opencode.nix
+++ dendritic-modules/programs/opencode/opencode.nix
@@ -1,8 +1,31 @@
 { ... }:
 {
   flake.homeModules.opencode =
-    { ... }:
+    { pkgs, ... }:
     {
+      home.packages = [
+        # Waiting for #554904 (https://github.com/NixOS/nixpkgs/pull/554904)
+        (pkgs.openspec.overrideAttrs (
+          finalAttrs: _previousAttrs: {
+            version = "1.10.0";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "Fission-AI";
+              repo = "OpenSpec";
+              tag = "v${finalAttrs.version}";
+              hash = "sha256-2wD99+Ma1VZKWG7CJeHDbuT3Td40b/4o0TDKEyQNzBQ=";
+            };
+
+            pnpmDeps = pkgs.fetchPnpmDeps {
+              inherit (finalAttrs) pname version src;
+              pnpm = pkgs.pnpm_10;
+              fetcherVersion = 3;
+              hash = "sha256-n+tFm3GvMV3vH2A+1LTJbqxzgk5wOCJ7kng/0y56Zlk=";
+            };
+          }
+        ))
+      ];
+
       programs.opencode = {
         enable = true;
         settings = {

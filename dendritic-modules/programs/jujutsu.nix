87745a03e24d680550fad4815d4e13306619eb20
Michael Andreas Graversen <home@michael-graversen.dk>
2026-08-30 11:46:05 +0200

    add jjui parallelize action

--- dendritic-modules/programs/jujutsu.nix
+++ dendritic-modules/programs/jujutsu.nix
@@ -19,6 +19,35 @@
         settings = {
           preview.show_at_start = true;
           ssh.hijack_askpass = true;
+          actions = [
+            {
+              name = "parallelize-selected";
+              desc = "parallelize selected changes";
+              key = "alt+p";
+              scope = "revisions";
+              lua = ''
+                local change_ids = revisions.checked()
+                if #change_ids < 2 then
+                  flash({ text = "Select at least two changes", error = true })
+                  return
+                end
+
+                local args = { "parallelize" }
+                for _, change_id in ipairs(change_ids) do
+                  table.insert(args, change_id)
+                end
+
+                local _, err = jj(args)
+                if err then
+                  flash({ text = err, error = true, sticky = true })
+                  return
+                end
+
+                flash("Parallelized " .. #change_ids .. " changes")
+                revisions.refresh({ selected_revision = change_ids[1] })
+              '';
+            }
+          ];
         };
       };
     };

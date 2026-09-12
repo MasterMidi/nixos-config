6e5cfd506a21412779ba5dc9918e912f99fa9c49
Michael Andreas Graversen <home@michael-graversen.dk>
2026-09-12 14:05:41 +0200

    chore(update): update immich version 3.2.0

--- k8s/immich.nix
+++ k8s/immich.nix
@@ -1,12 +1,12 @@
-{ config, lib, ... }:
+{ lib, ... }:
 let
   app = "immich";
   namespace = "immich";
-  version = "v3.1.0";
+  version = "v3.2.0";
 
   imageServer = "ghcr.io/immich-app/immich-server:${version}";
   imageML = "ghcr.io/immich-app/immich-machine-learning:${version}";
-  imageRedis = "docker.io/valkey/valkey:9@sha256:8e8d64b405ce18f41b8e5ee20aa4687a8ed0022d1298f2ce31cdcf3a76e09411";
+  imageRedis = "docker.io/valkey/valkey:9@sha256:4963247afc4cd33c7d3b2d2816b9f7f8eeebab148d29056c2ca4d7cbc966f2d9";
   imagePostgres = "ghcr.io/immich-app/postgres:14-vectorchord0.4.3-pgvectors0.2.0@sha256:bcf63357191b76a916ae5eb93464d65c07511da41e3bf7a8416db519b40b1c23";
 
   mkMLDaemonSet =

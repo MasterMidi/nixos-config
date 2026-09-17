2bd2785298d06c425b7e504934501d76969e41d0
Michael Andreas Graversen <home@michael-graversen.dk>
2026-09-17 07:38:59 +0200

    enable earlyoom

--- hosts/meridian/system.nix
+++ hosts/meridian/system.nix
@@ -4,6 +4,19 @@
 
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
+  services.earlyoom = {
+    enable = true;
+    freeMemThreshold = 15;
+    freeMemKillThreshold = 10;
+    # Do not let free disk swap postpone intervention until the desktop is unresponsive.
+    freeSwapThreshold = 100;
+    freeSwapKillThreshold = 100;
+    extraArgs = [
+      "--avoid"
+      "(^|/)(Hyprland)$"
+    ];
+  };
+
   # Bootloader
   boot.loader = {
     efi.canTouchEfiVariables = true;
